b66d44faab0f3bc755abc5c28b972bd7b60f9125
Mohammad Mahdi Keshavarz <MohammadMahdiKeshavarz1382@gmail.com>
2025-12-24 17:29:03 +0330

    Refactor DeployDSC script to remove mock collateral and streamline deployment logic

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -4,49 +4,25 @@ pragma solidity ^0.8.30;
 import {Script, console} from "forge-std/Script.sol";
 import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "../src/DSCEngine.sol";
-import {MockCollateral} from "../script/mocks/wbtcMock.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
-// (address[] memory collateralAddresses, address[] memory priceFeedAddresses, address dscTokenAddress)
 contract DeployDSC is Script {
+    address[] collaterals;
+    address[] priceFeeds;
     DSCEngine dscEngine;
     DecentralizedStableCoin dscToken;
-    MockCollateral wBTC;
-    MockCollateral wETH;
 
-    address btcPriceFeed = 0x5fb1616F78dA7aFC9FF79e0371741a747D2a7F22;
-    address ethPriceFeed = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
-    address[] collateralAddresses;
-    address[] priceFeedAddresses = [btcPriceFeed, ethPriceFeed];
-    address dscTokenAddress;
 
-    address constant PLAYER = address(0x100);
+    function run() external returns (DecentralizedStableCoin, DSCEngine) {
+        HelperConfig config = new HelperConfig();
+        (address wethPriceFeed, address wbtcPriceFeed, address weth, address wbtc, uint256 deployerKey) = config.activeNetworkConfig();
+        collaterals = [weth, wbtc];
+        priceFeeds = [wethPriceFeed, wbtcPriceFeed];
 
-    function run() public {
-        vm.startBroadcast();
+        vm.startBroadcast(deployerKey);
         dscToken = new DecentralizedStableCoin(msg.sender);
-        wBTC = new MockCollateral();
-        wETH = new MockCollateral();
-        collateralAddresses.push(address(wBTC));
-        collateralAddresses.push(address(wETH));
-
-        dscEngine = new DSCEngine(collateralAddresses, priceFeedAddresses, dscTokenAddress);
-        console.log("Collaterals:");
-        console.log(collateralAddresses[0]);
-        console.log(collateralAddresses[1]);
-        console.log("Price Feeds:");
-        console.log(priceFeedAddresses[0]);
-        console.log(priceFeedAddresses[1]);
-
-        wBTC.mint(msg.sender, 10 ether);
-        wBTC.approve(address(dscEngine), type(uint256).max);
-        dscEngine.depositCollateral(address(wBTC), 1 ether);
-        console.log("Deposited 1 wBTC as collateral");
-        uint256 hf = dscEngine.getHealthFactor(msg.sender);
-        console.log("Health Factor:", hf);
-        uint256 btcValue = dscEngine.getUsdValue(btcPriceFeed, 1);
-        console.log("1 wBTC is worth $", btcValue / 1e8);
-
+        dscEngine = new DSCEngine(collaterals, priceFeeds, address(dscToken));
+        dscToken.transferOwnership(address(dscEngine));
         vm.stopBroadcast();
-        dscTokenAddress = address(dscToken);
     }
 }
