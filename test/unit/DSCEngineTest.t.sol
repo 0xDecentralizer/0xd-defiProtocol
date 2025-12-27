0a8a6aed29650b143e6c5ad488a5fafdae8900d3
Mohammad Mahdi Keshavarz <MohammadMahdiKeshavarz1382@gmail.com>
2025-12-28 02:03:38 +0330

    test: test the getUsdValue function

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -12,11 +12,20 @@ contract DSCEngineTest is Test {
     DSCEngine dscEngine;
     DecentralizedStableCoin dscToken;
     HelperConfig config;
+    address wethUsdPriceFeed;
+    address weth;
 
     function setUp() public {
         deployer = new DeployDSC();
         (dscToken, dscEngine, config) = deployer.run();
+        (wethUsdPriceFeed,, weth,, ) = config.activeNetworkConfig();
     }
 
+    function testGetUsdValue() public {
+        uint256 amount = 3;
+        uint256 expectedUsd = 9_000;
+        uint256 actualUsd = dscEngine.getUsdValue(weth, amount);
 
+        assertEq(expectedUsd, actualUsd, "Invalid USD calculation!");
+    }
 }
