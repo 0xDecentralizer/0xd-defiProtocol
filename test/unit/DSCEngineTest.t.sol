4910da20ad91e7a1be73ec9d07696ceb923fd5af
Mohammad Mahdi Keshavarz <MohammadMahdiKeshavarz1382@gmail.com>
2025-12-30 01:58:42 +0330

    feat: write a modifier to deposit behalf of USER for more readability and clarity.

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -6,19 +6,34 @@ import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
 contract DSCEngineTest is Test {
+    address immutable USER = makeAddr("user 1");
+
     DeployDSC deployer;
     DSCEngine dscEngine;
     DecentralizedStableCoin dscToken;
     HelperConfig config;
     address wethUsdPriceFeed;
+    address wbtcUsdPriceFeed;
     address weth;
+    address wbtc;
+    
+    modifier userDeposited10Eth() {
+        ERC20Mock(weth).mint(USER, 10 ether);
+        vm.startPrank(USER);
+        IERC20(weth).approve(address(dscEngine), 10 ether);
+        dscEngine.depositCollateral(weth, 10 ether);
+
+        _;
+    }
 
     function setUp() public {
         deployer = new DeployDSC();
         (dscToken, dscEngine, config) = deployer.run();
-        (wethUsdPriceFeed,, weth,, ) = config.activeNetworkConfig();
+        (wethUsdPriceFeed, wbtcUsdPriceFeed, weth, wbtc, ) = config.activeNetworkConfig();
     }
 
     function testGetUsdValue() public {
