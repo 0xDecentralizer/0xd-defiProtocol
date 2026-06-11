147304d7b9516fc2512d94ea0903a4046f99da0f
Mohammad Mahdi Keshavarz <MohammadMahdiKeshavarz1382@gmail.com>
2026-06-11 12:24:57 +0330

    test: invariant test for depositCollateral function

---
+++ test/fuzz/Invariants.t.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.30;
+
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import {Handler} from "../handler/Handler.t.sol";
+
+contract InvariantTest is StdInvariant, Test {
+    DeployDSC deployer;
+    DSCEngine dsce;
+    DecentralizedStableCoin dsc;
+    HelperConfig config;
+    address weth;
+    address wbtc;
+    address wethUsdPriceFeed;
+    address wbtcUsdPriceFeed;
+    Handler handler;
+
+    function setUp() external {
+        deployer = new DeployDSC();
+        (dsc, dsce, config) = deployer.run();
+        (wethUsdPriceFeed, wbtcUsdPriceFeed ,weth, wbtc,) = config.activeNetworkConfig();
+        handler = new Handler(dsce, dsc, config);
+        targetContract(address(handler));
+    }
+
+    function invariant_testCollateralValueShouldBeGreaterThanTotalDscSupply() public {
+        uint256 totalWeth = ERC20Mock(weth).balanceOf(address(dsce));
+        uint256 totalWbtc = ERC20Mock(wbtc).balanceOf(address(dsce));
+        console.log("weth: ", totalWeth);
+        console.log("wbtc: ", totalWbtc);
+        uint256 totalDscSupply = dsc.totalSupply();
+        console.log("totalDsc: ", totalDscSupply);
+
+        uint256 wethUsdValue = dsce.getUsdValue(weth, totalWeth);
+        uint256 wbtcUsdValue = dsce.getUsdValue(wbtc, totalWbtc);
+        uint256 totalCollateralValue = wethUsdValue + wbtcUsdValue;
+
+        console.log("totalCollateralValue: ", totalCollateralValue);
+
+
+        assertGe(totalCollateralValue, totalDscSupply);
+    }
+}
